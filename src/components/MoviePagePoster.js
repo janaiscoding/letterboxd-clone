6f82726b990d08b495e83c725f902907383e65e0
janaiscoding <jana.istrate@gmail.com>
2023-05-13 00:36:04 +0300

    Add pseudocode for favourite handling

--- src/components/MoviePagePoster.js
+++ src/components/MoviePagePoster.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { auth } from "../firebase/firebase";
 import { db } from "../firebase/firebase";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 const MoviePagePoster = ({ movie }) => {
+  const [canFavourite, setCanFavourite] = useState(true);
+
   const onFavourite = async () => {
     // const userRef = collection(db, "users");
     if (auth.currentUser === null) {
       alert("login to use this feature");
     } else {
-      console.log(`adding to fav`, movie.title);
+      console.log('my user is logged in, now checking his existing collection')
       const userRef = collection(db, auth.currentUser.uid);
-      await addDoc(userRef, {
-        movie: movie,
-      });
+      //checking my existing docs
+      const query = await getDocs(userRef);
+      query.forEach((doc) =>{
+        console.log(canFavourite)
+        if(doc.data().movie.id === movie.id){
+          console.log(doc.data())
+          console.log('movie was found based on id and not name! you cant add it to favs already')
+          console.log('setting my boolean value to false')
+          setCanFavourite(false);
+          console.log('why does it not set to false wtf!!')
+          console.log(canFavourite)
+          console.log("returning as soon as it was found")
+          return;
+        }
+        return;
+      })
+     if(canFavourite){
+      console.log('waiting for checking to finish, havent found the movie in my sets, so i can fav = ', canFavourite)
+        await addDoc(userRef, {
+       movie: movie,
+       isFavourite: true,
+        });
+        }
     }
     console.log("called til the end");
   };
