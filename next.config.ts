7be6e25f68d17cc5d95c167709411d08cc6d23cb
janaiscoding <jana.istrate@gmail.com>
2025-03-02 16:57:17 +0100

    Adjust next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export',
-  distDir: 'build',
-  images: { 
-    unoptimized:true,
-  }
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
